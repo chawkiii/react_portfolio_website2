249324db49b16aabf3513dc44768257a2b6cb63a
chawki <achouriichawkii@gmail.com>
2024-09-27 14:03:08 +0100

    responsive about

--- src/playerStats/PlayerStats.js
+++ src/playerStats/PlayerStats.js
@@ -4,12 +4,7 @@ import "../styles/playerStats.css";
 const PlayerStats = () => {
   return (
     <div id='playerStats'>
-      <h1>Chawkii LVL 40</h1>
-      <div id='playerStats__lines'>
-        <span id='playerStats__lines__thick'></span>
-        <span id='playerStats__lines__thin'></span>
-      </div>
-      <h2>Fullstack Developer</h2>
+      <h1>Chawki Web Developer</h1>
     </div>
   );
 };
