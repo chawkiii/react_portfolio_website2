00f6a8820a9daea56e486ff53b34321144c19d2f
chawki <achouriichawkii@gmail.com>
2024-09-28 22:07:57 +0100

    css en gros

--- src/about/AboutMenuItem.js
+++ src/about/AboutMenuItem.js
@@ -4,7 +4,10 @@ import "../styles/aboutMenu.css";
 
 const AboutMenuItem = ({title, active, onClick}) => {
   return (
-    <div className={classNames("item", {active})} onClick={onClick}>
+    <div 
+      className={classNames("item", {active})} 
+      onClick={onClick}
+    >
       <h2 className='title'>{title}</h2>
     </div>
   )
--- src/projects/projectsData.js
+++ src/projects/projectsData.js
@@ -4,18 +4,19 @@ import projectThree from "../assets/project-3.png";
 
 const projects = {
   1: {
-    title: "SoMe Platform",
+    title: "Template Website Lawyer",
     image: projectOne,
     description: (
       <>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Using "HTML, CSS, JavaScript, Json":
+          SEO, Contact Form, Maps link, Responsive...
+          Designed to represent a lawyer specializing in business law and international law. Its main objective is to provide clear and accessible information about the legal services offered, while allowing users to easily contact him.
         </p>
       </>
     ),
-    github: "https://github.com",
-    demo: "https://netlify.com",
+    github: "https://github.com/chawkiii/avocat",
+    demo: "https://chawkii-avocat-temp.netlify.app/",
   },
   2: {
     title: "Productivity App",
